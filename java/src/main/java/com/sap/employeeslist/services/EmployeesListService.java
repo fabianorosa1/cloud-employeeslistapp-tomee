f918c40c939dc983e4d052ea67a8f64cb9110ab3
adop <adop@ldap.example.com>
2017-12-11 19:26:19 +0000

    push security backend code

--- java/src/main/java/com/sap/employeeslist/services/EmployeesListService.java
+++ java/src/main/java/com/sap/employeeslist/services/EmployeesListService.java
@@ -1,7 +1,16 @@
 package com.sap.employeeslist.services;
 
-import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class EmployeesListService
@@ -10,6 +19,21 @@ import javax.servlet.annotation.WebInitParam;
 		@WebInitParam(name= "javax.ws.rs.Application", value="org.apache.olingo.odata2.core.rest.app.ODataApplication"),
 		@WebInitParam(name= "org.apache.olingo.odata2.service.factory", value= "com.sap.employeeslist.odata.EmployeesListServiceFactory"),}
 )
-public class EmployeesListService extends org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet {
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"Display"}))
+
+public class EmployeesListService extends HttpServlet {
 	private static final long serialVersionUID = 5689120501367518278L;
-}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CoreService c;
+		try {
+			c = new CoreService();
+			c.getEmployees();
+		
+		
+		response.getWriter().println(c.getEmployees()); //request.getUserPrincipal()
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}}
